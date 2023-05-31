dbff66dfde264a39c75d62c71eb4d8eaa780a8af
Fardeen2001 <1si20et403@sit.ac.in>
2023-05-31 15:20:48 +0530

    filter Added

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let form=document.getElementById("addForm");
 let items=document.getElementById("items");
+let filter=document.getElementById("filter")
 
 form.addEventListener("submit",additems)
 items.addEventListener("click",removeitem)
+filter.addEventListener("keyup",search)
 
 function additems(e){
     e.preventDefault();
-    let submitval=document.getElementById("item").value;
+    let itemval=document.getElementById("item1").value;
+    let descval=document.getElementById("item2").value;
+
     let li=document.createElement("li")
     li.className="list-group-item"
-    li.appendChild(document.createTextNode(submitval));
+    li.appendChild(document.createTextNode(itemval));
+    li.appendChild(document.createTextNode(`-${descval}`));
 
     let delbtn=document.createElement('button');
     delbtn.className="btn btn-danger btn-sm float-sm-end delete ms-2";
@@ -31,4 +36,18 @@ function removeitem(e){
             items.removeChild(remli)
         }
     }
+}
+function search(e){
+    let text=e.target.value.toLowerCase();
+    let liItem=items.getElementsByTagName("li");
+    Array.from(liItem).forEach(function(item){
+        var itemName1=item.firstChild.textContent;
+        var itemName2=item.firstChild.nextSibling.textContent;
+        if(itemName1.toLowerCase().indexOf(text)!=-1 || itemName2.toLowerCase().indexOf(text)!=-1){
+            item.style.display="block";
+        }
+        else{
+            item.style.display="none"
+        }
+    });
 }
